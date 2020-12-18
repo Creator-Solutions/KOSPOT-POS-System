ac13603de851120c2d196bba18a363421ab06cea
Owen Burns <54483520+Creator-Solutions@users.noreply.github.com>
2020-12-18 20:07:16 +0200

    Added Files Via Upload
    Finished Project of the Point of Sales System

---
+++ Kospot POS Build 16.26.1.3/Kospot POS Build 16.26.1.3/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLogin))
+        Me.txtAdminName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblDateTime = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PBDBCon = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.PBDBCon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtAdminName
+        '
+        Me.txtAdminName.Location = New System.Drawing.Point(119, 146)
+        Me.txtAdminName.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtAdminName.Name = "txtAdminName"
+        Me.txtAdminName.Size = New System.Drawing.Size(148, 26)
+        Me.txtAdminName.TabIndex = 0
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(119, 215)
+        Me.txtPassword.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(148, 26)
+        Me.txtPassword.TabIndex = 1
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(155, 249)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 33)
+        Me.btnLogin.TabIndex = 2
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(142, 121)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Admin Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(128, 190)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Admin Password"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblDateTime})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 285)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
+        Me.StatusStrip1.Size = New System.Drawing.Size(397, 22)
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblDateTime
+        '
+        Me.lblDateTime.Name = "lblDateTime"
+        Me.lblDateTime.Size = New System.Drawing.Size(119, 17)
+        Me.lblDateTime.Text = "ToolStripStatusLabel1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'PBDBCon
+        '
+        Me.PBDBCon.Image = Global.Kospot_POS_Build_16._26._1._3.My.Resources.Resources.wifi
+        Me.PBDBCon.Location = New System.Drawing.Point(362, 2)
+        Me.PBDBCon.Name = "PBDBCon"
+        Me.PBDBCon.Size = New System.Drawing.Size(33, 30)
+        Me.PBDBCon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PBDBCon.TabIndex = 6
+        Me.PBDBCon.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(146, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(96, 94)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'frmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Aquamarine
+        Me.ClientSize = New System.Drawing.Size(397, 307)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.PBDBCon)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtAdminName)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.MaximizeBox = False
+        Me.Name = "frmLogin"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Log into Kospot Software"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.PBDBCon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtAdminName As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PBDBCon As PictureBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lblDateTime As ToolStripStatusLabel
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Kospot POS Build 16.26.1.3/Kospot POS Build 16.26.1.3/Form1.vb
@@ -0,0 +1,46 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class frmLogin
+
+    Dim MySqlCon As New MySqlConnection("host=127.0.0.1; user=root; password=")
+
+    Public adminName As String
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        txtPassword.PasswordChar = "*"
+        Dim SqlQuery As String = "SELECT AdminName, AdminPassword FROM kospot_db.admin WHERE AdminName='" + txtAdminName.Text + "' AND AdminPassword='" + txtPassword.Text + "'"
+
+        Dim SQLComm As New MySqlCommand
+
+        SQLComm.Connection = MySqlCon
+        SQLComm.CommandText = SqlQuery
+
+        Dim sqlReader As MySqlDataReader
+        sqlReader = SQLComm.ExecuteReader()
+
+        If sqlReader.Read() = True Then
+            adminName = sqlReader.GetValue(0).ToString()
+            MessageBox.Show("Welcome " + adminName)
+            Dim pos As New PointOfSale()
+            pos.ShowDialog()
+            Me.Hide()
+        End If
+
+        MySqlCon.Close()
+
+    End Sub
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtPassword.PasswordChar = "*"
+        Timer1.Enabled = True
+        Try
+            MySqlCon.Open()
+            PBDBCon.Image = My.Resources.wifi
+        Catch ex As Exception
+            PBDBCon.Image = My.Resources.disc
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        lblDateTime.Text = Date.Now.ToString("ddd dd MMM yyyy HH:mm:ss")
+    End Sub
+End Class
